95c4a6b16ff6bd4783d5c2c144143e858002716c
Saud Al-Slimany <saudalslimany@gmail.com>
2021-10-04 15:38:41 +0300

    Update sever.test.js

--- __test__/sever.test.js
+++ __test__/sever.test.js
@@ -3,9 +3,9 @@ import 'regenerator-runtime/runtime'
 const req = require('supertest')
 const app = require('../src/server/index')
 // The describe() function
-describe("Get", () => {
+describe("Get the api key", () => {
    // The it() function
-   it("function ", async () => {
+   it("function to check the key", async () => {
       const res = await req(app)
          .get('/getgeonamesAPIKey')
          .expect('saudc21')
