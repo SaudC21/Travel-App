28a7d3f83cbb0a9f54257455484f660f935f00ba
SaudC21 <saudalslimany@gmail.com>
2021-10-04 12:25:10 +0300

    Debugging
    Unauthorized by geonames API

--- src/client/js/geonamesAPI.js
+++ src/client/js/geonamesAPI.js
@@ -12,6 +12,9 @@ let cordinates = {
 
 // This function will return the cordinates
 async function getCordinates(apiKey) {
+    console.log(`
+        GeonamesAPI L16 ${apiKey.GEONAMES_USERNAME}
+    `)
     const apiCall = `http://api.geonames.org/searchJSON?q=${destination.value}&maxRows=1&username=${apiKey.GEONAMES_USERNAME}`;
     geonamesObject = await getApiCall(apiCall)
     if (geonamesObject.totalResultsCount == 0) {
@@ -32,6 +35,9 @@ async function getCordinates(apiKey) {
 
 async function cordinatesHandler() {
     try {
+        console.log(`
+            cordinatesHandler
+        `)
         let route = '/getApiKey'
         await getApiKey(route)
             .then(async function (apiKey) {
